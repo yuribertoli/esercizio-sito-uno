fd6c561c4c2a223a4ca43c7bfddbe81720f40f78
yuribertoli <yuribertoli@yahoo.it>
2022-09-14 18:08:34 +0200

    vue cli

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
