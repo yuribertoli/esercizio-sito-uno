ae7a8735997691e64acff20f0c90b36e52d7b2d9
yuribertoli <yuribertoli@yahoo.it>
2022-09-16 18:00:14 +0200

    header done

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -22,24 +22,33 @@
             @mouseout="isHovering = false"
             :class="isHovering ? 'fa fa-user' : 'fa fa-sign-in'" 
         ></i>
-        <!-- 
-            v-show="isHovering"
+ 
+        <!-- compare all'hover dell'icona sopra, resta attivo finché c'è hover -->
+        <div v-show="isHovering"
             @mouseover="isHovering = true" 
             @mouseout="isHovering = false"
-         -->
-        <!-- compare all'hover dell'icona sopra, resta attivo finché c'è hover -->
-        <div 
             id="sign-in-up">
-
+            
             <div id="talkbubble"> <!-- box sign-in o sign-up all'hover sull'icona -->
+
+                <div id="talkbubbletriangle" 
+                :class="colorTriangle ? 'sameTriangleTrue' : 'sameTriangleFalse'"></div>
 
                 <ul>
                     <li @mouseover="colorTriangle = true"
                         @mouseout="colorTriangle = false" 
                         id="sign-in">
-                        <a href="#">Sign-In</a>
+                        <a href="#">
+                            <i class="fa fa-hand-peace-o"></i>
+                            Sign-In
+                        </a>
                     </li>
-                    <li id="sign-up"><a href="#">Sign-Up</a></li>
+                    <li id="sign-up">
+                        <a href="#">
+                            <i class="fa fa-hand-spock-o"></i>
+                            Sign-Up
+                        </a>
+                    </li>
                 </ul>
 
             </div>
@@ -67,6 +76,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/style/variables.scss";
+
 header {
     height: 70px;
     width: 100%;
@@ -119,7 +130,7 @@ header {
 
         i {
             font-size: 1.5rem;
-            color: rgb(66, 175, 194);
+            color: $colorMouseOut;
 
             &:hover {
                 cursor: pointer;
@@ -136,7 +147,7 @@ header {
             #talkbubble { //box di messaggio sign-in e sign-up
                 width: 120px;
                 height: 80px;
-                background: rgb(66, 175, 194);
+                background: $colorMouseOut;
                 position: relative;
                 top: 10px;
                 right: 50px;
@@ -153,7 +164,7 @@ header {
                     width: 100%;
 
                     #sign-in a{
-                        border-radius: 10px 10px 0 0; 
+                        border-radius: 10px 0 0 0; 
                     }
 
                     #sign-up a{
@@ -175,24 +186,38 @@ header {
 
                             &:hover {
                                 text-decoration: underline;
-                                background-color: rgb(61, 164, 183);
+                                background-color: $colorMouseOver;
                             }
+                        }
+
+                        i {
+                            color: white;
+                            font-size: 0.9rem;
                         }
                     }
                 }
             }
-                #talkbubble:before { //triangolo in alto a destra
+
+            //per colorare il triangolo in alto a destra dello stesso colore dell'hover
+            .sameTriangleFalse { 
+                border-top: 20px solid $colorMouseOut;
+            }
+
+            .sameTriangleTrue {
+                border-top: 20px solid $colorMouseOver;
+            }
+
+            #talkbubbletriangle { //triangolo in alto a destra
                 content: "";
                 position: absolute;
                 left: 100%;
                 top: 0px;
                 width: 0;
                 height: 0;
-                border-top: 20px solid rgb(66, 175, 194);
                 border-right: 20px solid transparent;
 
                 #triangle-color {
-                    border-top: 20px solid rgb(61, 164, 183);
+                    border-top: 20px solid $colorMouseOver;
                 }
             }
         }
